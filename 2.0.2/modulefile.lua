1d8f1744ccde47927fde8267600e3275afb4e2ee
icaoberg <icaoberg@andrew.cmu.edu>
2021-06-28 11:44:08 -0400

    Update modulefile.lua

--- 2.0.2/modulefile.lua
+++ 2.0.2/modulefile.lua
@@ -3,37 +3,36 @@
 --
 -- "URL: https://www.psc.edu/resources/software"
 -- "Category: Other"
--- "Description: is a free and open source solution for recording terminal sessions and sharing them on the web."
+-- "Description: asciinema is a free and open source solution for recording terminal sessions and sharing them on the web."
 -- "Keywords: singularity bioinformatics"
 
 whatis("Name: asciinema")
 whatis("Version: 2.0.2")
 whatis("Category: Other")
 whatis("URL: https://www.psc.edu/resources/software")
-whatis("Description:  is a free and open source solution for recording terminal sessions and sharing them on the web.")
+whatis("Description: asciinema is a free and open source solution for recording terminal sessions and sharing them on the web.")
 
 help([[
-asciinema 2.0.2
----------------
 Description
 -----------
-asciinema, a pyhton-based analysis pipeline for
-* (single-cell) (whole-genome) bisulfite sequencing data
-* (single-cell) NOMe-seq data
-* differential methylation analysis
+asciinema is a free and open source solution for recording terminal sessions and sharing them on the web.
+  
 To load the module type
+  
 > module load asciinema/2.0.2
+
 To unload the module type
+
 > module unload asciinema/2.0.2
+
 Documentation
 -------------
 For help, type
-> methylpy --help
-Repository
-----------
-https://github.com/yupenghe/asciinema
+
+> asciinema --help
+
 Tools included in this module are
-* methylpy
+* asciinema
 ]])
 
 local package = "asciinema"
